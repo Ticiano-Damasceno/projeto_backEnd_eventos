14854b2e72b8270fbc4debd57036fb6e73fc223e
Ticiano <ticiano.damasceno@hotmail.com>
2024-08-10 18:09:37 -0300

    Vídeo 01: código do vídeo completo

---
+++ init/consulta_banco.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, sql
+
+engine = create_engine('sqlite:///storage.db', echo=True)
+conn = engine.connect()
+
+query = sql.text(
+    'SELECT * FROM tbl_events'
+)
+
+result = conn.execute(query).fetchall()
+print(result)
---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,30 @@
+import os
+print(os.getcwd())
+
+
+import pytest
+from src.models.settings.connection import db_connection_handler  
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason='Novo registro em banco de dados')
+def test_insert_event():
+    event_random = {
+        'uuid': 'meu-uuid-testando',
+        'title': 'meu titulo',
+        'slug': 'meu slug', 
+        'maximum_attendees': 20
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event_random)
+    print(response)
+
+# @pytest.mark.skip(reason="já testado e funcionou!")
+def test_get_event_by_id():
+    event_id = 'meu-uuid-testando'
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id=event_id)
+    print(response)
+
